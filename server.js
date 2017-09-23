9d817717260aaa57784f30c333ca7df252ffd70f
John O'Connell <johnoconnell@Johns-MacBook-Pro.local>
2017-09-23 17:02:30 -0500

    added the summary and made some html changes

--- server.js
+++ server.js
@@ -52,18 +52,28 @@ app.get("/scrape", function(req, res) {
   request("http://spacenews.com/segment/news/", function(error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
+
+//Lauren gave me permission to do this :) 
+     var result = {}
+
     // Now, we grab every h2 within an article tag, and do the following:
     $("h2.launch-title").each(function(i, element) {
-
-      // Save an empty result object
-      var result = {};
-
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this).children("a").text();
       result.link = $(this).children("a").attr("href");
 
+})
+
+//add in summary each function 
+     $("p.post-excerpt").each(function(i, element) {
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.summary = $(this).text();
+      console.log(this);
+})
+
       // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
+      // This effectively passes  the result object to the entry (and the title and link)
       var entry = new Article(result);
 
       // Now, save that entry to the db
@@ -77,12 +87,14 @@ app.get("/scrape", function(req, res) {
           console.log(doc);
         }
       });
-
-    });
+  
   });
   // Tell the browser that we finished scraping the text
   res.send("Scrape Complete");
 });
+
+
+
 
 // This will get the articles we scraped from the mongoDB
 app.get("/articles", function(req, res) {
@@ -98,6 +110,9 @@ app.get("/articles", function(req, res) {
     }
   });
 });
+
+
+
 
 // Grab an article by it's ObjectId
 app.get("/articles/:id", function(req, res) {
